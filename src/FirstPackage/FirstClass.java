141d23ff80b45f060abc8cf6fadbb239600f4e89
hilcat <hilalcatpinar@gmail.com>
2021-06-24 17:53:23 -0400

    Second Commit

--- src/FirstPackage/FirstClass.java
+++ src/FirstPackage/FirstClass.java
@@ -3,5 +3,6 @@ package FirstPackage;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("First Commit of the project");
+        System.out.println("Second Commit from local master");
     }
 }
